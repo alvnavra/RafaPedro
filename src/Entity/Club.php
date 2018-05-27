e8ce38a08ef043e5f9294869b7540544f7e412af
alvnavra <alvnavra@gmail.com>
2018-05-27 10:22:05 +0200

    proyecto con Doctrine

---
+++ src/Entity/Club.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ClubRepository")
+ */
+class Club
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $bussinessNumber;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getBussinessNumber(): ?string
+    {
+        return $this->bussinessNumber;
+    }
+
+    public function setBussinessNumber(string $bussinessNumber): self
+    {
+        $this->bussinessNumber = $bussinessNumber;
+
+        return $this;
+    }
+}
